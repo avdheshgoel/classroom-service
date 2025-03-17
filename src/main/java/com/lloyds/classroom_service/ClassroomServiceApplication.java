85a42c2657354da33eff2838457f66151e00d6e4
Avdhesh Goel <avdheshgoel@Avdheshs-MacBook-Pro-2.local>
2025-03-17 10:10:51 +0000

    asd

--- src/main/java/com/lloyds/classroom_service/ClassroomServiceApplication.java
+++ src/main/java/com/lloyds/classroom_service/ClassroomServiceApplication.java
@@ -1,13 +1,43 @@
 package com.lloyds.classroom_service;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Collection;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class ClassroomServiceApplication {
 
+	private Collection<ClassroomServiceApplication> friends;
+	private String email;
+
+	public ClassroomServiceApplication(String email) {
+		this.friends = friends;
+		this.email = email;
+	}
+
+	public Collection<ClassroomServiceApplication> getClassroomServiceApplication() {
+		return friends;
+	}
+
+	public String getEmail(String email) {
+		return email;
+	}
+
+
+	public void addFriendShip(ClassroomServiceApplication friend) {
+		friends.add(friend);
+		friend.getClassroomServiceApplication().add(this);
+	}
+
+	public boolean canBeConnected(ClassroomServiceApplication classroomServiceApplication) {
+		throw new UnsupportedOperationException("asdasd");
+	}
+
 	public static void main(String[] args) {
-		SpringApplication.run(ClassroomServiceApplication.class, args);
+		ClassroomServiceApplication a = new ClassroomServiceApplication("A");
+		ClassroomServiceApplication b = new ClassroomServiceApplication("A");
+		ClassroomServiceApplication c = new ClassroomServiceApplication("A");
+		a.addFriendShip(b);
+		b.addFriendShip(c);
+//		SpringApplication.run(ClassroomServiceApplication.class, args);
 	}
 
 }
