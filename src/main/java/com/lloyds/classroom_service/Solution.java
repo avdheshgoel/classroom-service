5627956097098eec8b7acdcd78881cabfcb2c0ed
Avdhesh Goel <avdheshgoel@Avdheshs-MacBook-Pro-2.local>
2025-03-18 11:07:57 +0000

    asd

---
+++ src/main/java/com/lloyds/classroom_service/Solution.java
@@ -0,0 +1,13 @@
+package com.lloyds.classroom_service;
+
+import java.util.Collection;
+
+
+public class Solution {
+
+	public static String run(int input) {
+
+		String result= "";
+		return result;
+	}
+}
