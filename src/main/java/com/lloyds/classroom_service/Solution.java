13f7cc67439cc6f50d567ded5a2833c6a26b1e31
Avdhesh Goel <avdheshgoel@Avdheshs-MacBook-Pro-2.local>
2025-03-21 14:09:35 +0000

    asd

--- src/main/java/com/lloyds/classroom_service/Solution.java
+++ src/main/java/com/lloyds/classroom_service/Solution.java
@@ -1,13 +1,29 @@
 package com.lloyds.classroom_service;
 
-import java.util.Collection;
+import java.util.*;
+import java. lang.*;
+import java.io.*;
 
+public class Solution
+{
+	public static int calculateDesktopProductIDs(char[] productID) {
+		int answer = 0;
 
-public class Solution {
-
-	public static String run(int input) {
-
-		String result= "";
-		return result;
+		return answer;
 	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner (System. in);
+		// input for productID
+		int productID_size = in.nextInt();
+		char productID[] = new char[productID_size]:
+		for(int idx = 0; idx < productID_size; idx++) {
+			productID [idx] = in.next(). charAt(8);
+			int result = calculateDesktopProductIDs (productID) ;
+			System.out.print(result);
+		}
+
+	}
+
 }
+
