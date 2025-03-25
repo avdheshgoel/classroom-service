e003e29cca44514b2cb5bb2634ce3bb1c963c838
Avdhesh Goel <avdheshgoel@Avdheshs-MacBook-Pro-2.local>
2025-03-25 14:21:39 +0000

    asd

--- src/main/java/com/lloyds/classroom_service/Solution.java
+++ src/main/java/com/lloyds/classroom_service/Solution.java
@@ -1,43 +1,13 @@
 package com.lloyds.classroom_service;
 
-import java.util.*;
-import java. lang.*;
+import java.util.Collection;
+
 
 public class Solution {
 
-		public static int countOfElement(int[] listInputl, int[] listInput2) {
-			int answer = 0;
-			// Write your code here
-			return answer;
-		}
+	public static String run(int input) {
 
-		public static void main(String[] args) {
-			Scanner in = new Scanner(System.in);
-			int listInput1_size = in.nextInt();
-			int listInput1[] = new int[listInput1_size];
-			for(int idx = 0; idx < listInput1_size; idx++) {
-				listInput1[idx] = in.nextInt();
-			}
-			// input for listInput2
-			int listInput2_size = in.nextInt();
-			int listInput2[] = new int [listInput2_size];
-			for(int idx = 0; idx < listInput2_size; idx++) {
-				listInput2[idx] = in.nextInt();
-			}
-			int result = countOfElement(listInput1, listInput2);
-			System.out.print(result);
-
-		}
+		String result= "";
+		return result;
+	}
 }
-
-
-
-
-
-
-
-
-
-
-		{
-
