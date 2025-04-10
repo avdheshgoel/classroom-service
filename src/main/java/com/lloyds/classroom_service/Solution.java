b255f8c9c22beec99232220e057b93b4447c5411
Avdhesh Goel <avdheshgoel@MacBookPro.lan>
2025-04-10 22:37:19 +0100

    asd

--- src/main/java/com/lloyds/classroom_service/Solution.java
+++ src/main/java/com/lloyds/classroom_service/Solution.java
@@ -1,13 +1,49 @@
 package com.lloyds.classroom_service;
 
+import java.io.*;
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.IntStream;
 
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
 
-public class Solution {
-
-	public static String run(int input) {
-
-		String result= "";
-		return result;
-	}
+static class Result {
 }
+
+public static List<String> getMessageStatus(List<Integer> timestamps, List<String> messages, int k) {
+
+}
+
+	public class Solution {
+		public static void main(String[] args) throws IOException {
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System. in)) ;
+			BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System. getenv("OUTPUT_PATH"))) ;
+			int timestampsCount = Integer.parseInt(bufferedReader.readLine(). trim());
+
+			List<Integer> timestamps = IntStream. range(0, timestampsCount).mapToObj (i -> {
+				try {
+					return bufferedReader.readLine().replaceAll("\\s+$", "");
+				} catch (IOException ex) {
+					throw new RuntimeException(ex);
+				}
+			}).map(String::trim)
+					.map(Integer:: parseInt)
+					.collect (toList());
+			int messagesCount = Integer. parseInt(bufferedReader. readLine(). trim()) ;
+			List<String> messages = IntStream. range (0, messagesCount).mapToObj (i -> {
+				try {
+					return bufferedReader.readLine();
+				} catch (IOException ex) {
+					throw new RuntimeException(ex);
+				}
+			}).collect(toList());
+
+			int k = Integer.parseInt(bufferedReader.readLine().trim()) ;
+			List<String> result = Result.getMessageStatus(timestamps, messages, k) ;
+			bufferedWriter.write( result.stream().collect (joining("\n")) + "\n");
+
+			bufferedReader.close();
+			bufferedWriter.close();
+		}
+	}
